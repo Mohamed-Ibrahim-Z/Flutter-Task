29d602fee62f7359831f80522a806335f319a894
Mohamed Ibrahim <mohmed42010@gmail.com>
2022-04-14 00:16:16 +0200

    Learning the home Screen

---
+++ lib/home_screen.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu),
+        title: Text('Home'),
+        actions: [
+          Icon(Icons.notification_important),
+        ],
+      ),
+    );
+  }
+}
