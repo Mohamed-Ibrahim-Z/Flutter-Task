93ab79b7092a62bd05fb8037ec18943570cf995a
Mohamed Ibrahim <mohmed42010@gmail.com>
2022-04-15 03:25:50 +0200

    Adding Button, Icons, Some Texts and ending the task

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,7 +11,6 @@ class HomeScreen extends StatelessWidget {
           foregroundColor: Colors.black,
           elevation: 0,
           leading: Icon(Icons.menu),
-          title: Text('Home'),
           actions: [
         IconButton(
           onPressed: () {},
@@ -31,14 +30,14 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.only(
-          top: 250
+          top: 220
         ),
-        child: Container(
+        child: SizedBox(
           width: double.infinity,
-
           child: Column(
             children: [
               Container(
+
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(
                       50
@@ -52,10 +51,179 @@ class HomeScreen extends StatelessWidget {
                   fit: BoxFit.cover,
                 ),
               ),
+              SizedBox(height: 15,),
+              Text(
+                  'Mohamed Ibrahim',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 25,
+                ),
 
+              ),
+              SizedBox(height: 15,),
+              Text(
+                'Im not intrested in anything',
+              ),
+              SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+
+                        '540',
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 30,
+                        ),
+                      ),
+                      Text(
+                        'Rating',
+                        style: TextStyle(
+
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 35,),
+                  Column(
+                    children: [
+                      Text(
+
+                        '700',
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                      Text(
+                        'Followers',
+                        style: TextStyle(
+
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 35,),
+                  Column(
+                    children: [
+                      Text(
+                        '142',
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                      Text(
+                        'Posts',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 15,),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                      'Subsecribe'
+                  ),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)
+                      )
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15,),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 20,
+                      ),
+                      child: Icon(
+                        Icons.location_on,
+                        color: Colors.purple,
+                      ),
+
+                    ),
+                    SizedBox(width: 15,),
+                    Text(
+                      'Lives in Cairo',
+                      style: TextStyle(
+                        fontSize: 15
+                      ),
+                    ),
+                  ],
+
+
+              ),
+              SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                    ),
+                    child: Icon(
+                      Icons.work,
+                      color: Colors.orange,
+                    ),
+
+                  ),
+                  SizedBox(width: 15,),
+                  Text(
+                    'Works as Member at OSC',
+                    style: TextStyle(
+                        fontSize: 15
+                    ),
+                  ),
+                ],
+
+
+              ),
+              SizedBox(height: 15,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                    ),
+                    child: Icon(
+                      Icons.star,
+                      color: Colors.blue,
+                    ),
+
+                  ),
+                  SizedBox(width: 15,),
+                  Text(
+                    'Skills Flutter, C++, Unity & other',
+                    style: TextStyle(
+                        fontSize: 15
+                    ),
+                  ),
+                ],
+
+
+              ),
             ],
           ),
         ),
+
       ),
     );
   }
